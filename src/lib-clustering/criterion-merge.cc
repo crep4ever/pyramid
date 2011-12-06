7843281c774dae1c774a011bdb379cbc017c991a
Romain Goffe <romain.goffe@gmail.com>
2011-12-06 14:16:25 +0100

    remove useless namespaces

--- src/lib-clustering/criterion-merge.cc
+++ src/lib-clustering/criterion-merge.cc
@@ -1,7 +1,5 @@
 //******************************************************************************
 #include "inline-macro.hh"
 #include "criterion-merge.hh"
-using namespace std;
 #include INCLUDE_NON_INLINE("criterion-merge.icc")
-using namespace Map2d;
 //******************************************************************************
