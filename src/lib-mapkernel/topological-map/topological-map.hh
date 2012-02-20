ccf436c4667257612d9e1328ce00428198b75aec
Romain Goffe <romain.goffe@gmail.com>
2012-02-20 18:08:06 +0100

    mapkernel: protected range for FNbRegions

--- src/lib-mapkernel/topological-map/topological-map.hh
+++ src/lib-mapkernel/topological-map/topological-map.hh
@@ -447,10 +447,10 @@ protected:
     CImage2D*   FImage;
     CKhalimsky* FKhalimsky;
     CRegion*    FInclusionTreeRoot;
-private:
     /// Compteurs pour le nombre de régions
     unsigned int FNbRegions;
 
+private:
     /// vrai si on est en mode uftree: dans ce cas, le champ Father de chaque
     /// région désigne le père dans l'arbre uf, null pour les racines.
     bool FUFTreeMode;  
