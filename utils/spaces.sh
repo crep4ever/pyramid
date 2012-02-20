31632ffd1751ee110bec3df699c070cdfe09a4f6
Romain Goffe <romain.goffe@gmail.com>
2012-02-20 18:13:46 +0100

    trailing spaces

--- utils/spaces.sh
+++ utils/spaces.sh
@@ -2,8 +2,8 @@
 #Description: remove trailing spaces
 
 sed -i 's/[ \t]*$//' src/ihm/*.*
-sed -i 's/[ \t]*$//' src/lib-mapkernel/pyramid/*.*
+sed -i 's/[ \t]*$//' src/lib-pyramid/*.*
 sed -i 's/[ \t]*$//' src/lib-utils/*.*
 sed -i 's/[ \t]*$//' src/lib-clustering/*.*
-sed -i 's/[ \t]*$//' src/lib-image2D/*.*
+sed -i 's/[ \t]*$//' src/lib-image2D/imageTiff.*
 sed -i 's/[ \t]*$//' cmake/modules/*.*
