9c8340ae67c338b83583c8803520cb516d98bfa6
Romain Goffe <romain.goffe@gmail.com>
2012-03-12 18:45:16 +0100

    utils: trailing spaces

--- utils/spaces.sh
+++ utils/spaces.sh
@@ -6,4 +6,6 @@ sed -i 's/[ \t]*$//' src/lib-pyramid/*.*
 sed -i 's/[ \t]*$//' src/lib-utils/*.*
 sed -i 's/[ \t]*$//' src/lib-clustering/*.*
 sed -i 's/[ \t]*$//' src/lib-image2D/imageTiff.*
+sed -i 's/[ \t]*$//' src/lib-mapkernel/*.*
+sed -i 's/[ \t]*$//' src/lib-mapkernel/*/*.*
 sed -i 's/[ \t]*$//' cmake/modules/*.*
